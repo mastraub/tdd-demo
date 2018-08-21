d4cd989bcc6a8de8b838e6330b749f04e4620f9a
Martin Straub <m.straub@henova.de>
2018-08-21 17:31:14 +0200

    add button click

--- TddDemo/WpfApp1/MainWindow.xaml.cs
+++ TddDemo/WpfApp1/MainWindow.xaml.cs
@@ -7,9 +7,17 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int _count;
+
         public MainWindow()
         {
             InitializeComponent();
+            TextBox.Text = _count.ToString();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text = (++_count).ToString();
         }
     }
 }
