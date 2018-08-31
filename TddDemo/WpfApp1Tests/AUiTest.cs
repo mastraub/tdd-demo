febce3a6115b5e83c9d219aec89ba51d7df37a70
Martin Straub <m.straub@henova.de>
2018-08-31 11:35:12 +0200

    extract abstract class AUiTest

---
+++ TddDemo/WpfApp1Tests/AUiTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestStack.White;
+using TestStack.White.UIItems.WindowItems;
+
+namespace WpfApp1Tests
+{
+    public abstract class AUiTest
+    {
+        // Aut = App under test
+
+        public const string AutName = "WpfApp1";
+
+        protected Window Window;
+
+        protected Application Application { get; private set; }
+
+        [TestInitialize]
+        public virtual void AUiTestInitialize()
+        {
+            Application = InitApp(AutName);
+        }
+
+        [TestCleanup]
+        public virtual void AUiTestCleanup()
+        {
+            Application.Close();
+            Application.Dispose();
+        }
+
+        protected void SetWindow(string name) =>
+            Window = Application.GetWindow(name);
+
+        protected Application InitApp(string name)
+        {
+            var executable = AutExecutablePath(name);
+            return Application.Launch(executable);
+        }
+
+        public static string AutExecutablePath(string projectName)
+        {
+            var codebase = Assembly.GetExecutingAssembly()
+                .CodeBase;
+            var executionPath = new Uri(codebase).AbsolutePath;
+            var stepOut = "../../../../"; // see next line: stepIn
+            var stepIn = $"{projectName}/bin/Debug/{projectName}.exe";
+            var navigate = Path.Combine(executionPath, stepOut, stepIn);
+            var appPath = Path.GetFullPath(navigate);
+            return appPath;
+        }
+    }
+}
--- TddDemo/WpfApp1Tests/UiTest1.cs
+++ TddDemo/WpfApp1Tests/UiTest1.cs
@@ -1,45 +1,23 @@
-﻿using System;
-using System.IO;
-using System.Reflection;
-using FluentAssertions;
+﻿using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using TestStack.White;
-using TestStack.White.Factory;
 using TestStack.White.UIItems;
 using TestStack.White.UIItems.Finders;
-using TestStack.White.UIItems.WindowItems;
 
 namespace WpfApp1Tests
 {
     [TestClass]
-    public class UiTest1
+    public class UiTest1 : AUiTest
     {
-        private Window _window;
-
         [TestInitialize]
         public void Initialize()
         {
-            var autExe = AutExecutablePath("WpfApp1");
-            var application = Application.Launch(autExe);
-            _window = application.GetWindow("MainWindow", InitializeOption.NoCache);
-        }
-
-        // Aut = App under test
-        public string AutExecutablePath(string projectName)
-        {
-            var codebase = Assembly.GetExecutingAssembly().CodeBase;
-            var executionPath = new Uri(codebase).AbsolutePath;
-            var stepOut = "../../../../"; // see next line: stepIn
-            var stepIn = $"{projectName}/bin/Debug/{projectName}.exe";
-            var navigate = Path.Combine(executionPath, stepOut, stepIn);
-            var appPath = Path.GetFullPath(navigate);
-            return appPath;
+            SetWindow("MainWindow");
         }
 
         [TestMethod]
         public void TextBox_Text_Is0()
         {
-            _window.Get<TextBox>("TextBox")
+            Window.Get<TextBox>("TextBox")
                 .Text.Should()
                 .Be("0");
         }
@@ -47,9 +25,9 @@ namespace WpfApp1Tests
         [TestMethod]
         public void ButtonClick_Click_TextBoxNumberFrom0to1()
         {
-            _window.Get<Button>(SearchCriteria.ByText("Button"))
+            Window.Get<Button>(SearchCriteria.ByText("Button"))
                 .Click();
-            _window.Get<TextBox>("TextBox")
+            Window.Get<TextBox>("TextBox")
                 .Text.Should()
                 .Be("1");
         }
